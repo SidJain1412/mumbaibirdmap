7944be885a8db7642ef3d2cb769dffea282b77be
Siddharth Jain <siddharth.jain@clevertap.com>
2024-11-26 14:16:23 +0530

    undo routing GA

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router';
 import "leaflet/dist/leaflet.css";
 import Multiselect from '@vueform/multiselect'
 import '@vueform/multiselect/themes/default.css'
 
 const app = createApp(App);
 app.component('v-select', Multiselect);
-
-router.afterEach((to) => {
-  gtag('config', 'G-VHEC5TXVJ2', {
-    page_path: to.fullPath,
-  });
-});
-
-app.use(router);
 app.mount("#app");
